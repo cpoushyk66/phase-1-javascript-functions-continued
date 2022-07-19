089c44ab35936243a7df7bc1bcce63f43a516daa
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-07-19 09:46:26 -0400

    commit tree dcd71d095bc5f4ca0a366ed17ded90dc85c39af1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(value = 'roller-skate') {
+    return `This Saturday, I want to ${value}!`;
+}
+
+function mondayWork(value = 'go to the office') {
+    return `This Monday, I will ${value}.`;
+}
+
+function wrapAdjective(value = '*') {
+    const txt = value;
+    return function(val = 'special') {
+        return `You are ${txt}${val}${txt}!`
+    }
+}
